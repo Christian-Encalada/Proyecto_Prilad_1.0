836bed10428eea59a1e02f242ad5ad0f9774811b
Christian-Encalada <cris_7715@hotmail.com>
2023-01-04 00:51:22 -0500

    Initial commit

---
+++ app/src/main/java/com/chris/proyecto_prilad/Pantalla_2.kt
@@ -0,0 +1,11 @@
+package com.chris.proyecto_prilad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla2)
+    }
+}
